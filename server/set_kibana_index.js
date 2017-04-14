40daadcadf256236ae5bbd64e1437c7aed689744
Mocker <lsin30@foxmail.com>
2017-04-14 13:44:43 +0800

    Update set_kibana_index.js
    set default index

--- server/set_kibana_index.js
+++ server/set_kibana_index.js
@@ -1,3 +1,14 @@
 export default function (server, request, remoteUser, suffix) {
-  request.yar.set(remoteUser, { key: server.config().get('kibana.index') + '_' + suffix });
+  const defaultIndex = server.config().get('own_home.default_kibana_index_suffix');
+  const prefix = server.config().get('kibana.index');
+  let idx = [prefix];
+  if (suffix) {
+    idx.push(suffix);
+  }
+
+  let value = idx.join('_');
+  const remoteUserSession = request.yar.get(remoteUser) || {};
+  const requestKbnIndex = remoteUserSession.key;
+  value = requestKbnIndex ? value : `${prefix}_${defaultIndex}`;
+  request.yar.set(remoteUser, {key: value});
 };
